76ce259b7a6a1981532f21d837a0daa832e7adc8
Jan Eisenhuth <47415874+Eisenhuth@users.noreply.github.com>
2024-07-27 05:38:03 +0200

    pre-emptive fix for path becoming optional

--- Sources/xivapi-swift/xivapi-api/Icon.swift
+++ Sources/xivapi-swift/xivapi-api/Icon.swift
@@ -2,11 +2,11 @@ import Foundation
 
 public struct Icon: Codable, Hashable {
     public let id: Int
-    public let path: String
-    public let path_hr1: String
+    public let path: String?
+    public let path_hr1: String?
 }
 
 public extension Icon {
-    var iconUrl: URL? { Endpoint.asset(at: path) }
-    var iconUrlHD: URL? { Endpoint.asset(at: path_hr1) }
+    var iconUrl: URL? { path != nil ? Endpoint.asset(at: path!) : nil }
+    var iconUrlHD: URL? { path_hr1 != nil ? Endpoint.asset(at: path_hr1!) : nil }
 }
