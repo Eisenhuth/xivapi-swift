791f268a187b01d1eb87bb58baf8bd996b50ae30
Jan Eisenhuth <47415874+Eisenhuth@users.noreply.github.com>
2024-07-28 02:31:00 +0200

    Update TripleTriadCard.swift

--- Sources/xivapi-swift/xivapi-api/TripleTriadCard.swift
+++ Sources/xivapi-swift/xivapi-api/TripleTriadCard.swift
@@ -9,3 +9,18 @@ public struct TripleTriadCardFields: Codable {
     public let Description: String
     public let Name: String
 }
+
+public extension TripleTriadCard {
+    
+    var backgroundUrl: URL? { Endpoint.asset(at: "ui/uld/CardTripleTriad_hr1.tex") }
+    var imageUrl: URL? {
+        
+        let cardNumber = row_id
+        let path = "ui/icon/087000/087\(cardNumber)_hr1.tex"
+        let url = Endpoint.asset(at: path)
+        
+        return url
+    }
+    
+    //TODO: stats, category
+}
