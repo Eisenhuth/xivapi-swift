b1e707dce4149f45f9e3d05b84e15ba42d8a9432
Jan Eisenhuth <47415874+Eisenhuth@users.noreply.github.com>
2024-07-28 03:28:55 +0200

    turns out padding is necessary

--- Sources/xivapi-swift/xivapi-api/TripleTriadCard.swift
+++ Sources/xivapi-swift/xivapi-api/TripleTriadCard.swift
@@ -15,8 +15,9 @@ public extension TripleTriadCard {
     var backgroundUrl: URL? { Endpoint.asset(at: "ui/uld/CardTripleTriad_hr1.tex") }
     var imageUrl: URL? {
         
-        let cardNumber = row_id
-        let path = "ui/icon/087000/087\(cardNumber)_hr1.tex"
+        let cardNumber = row_id.description
+        let paddedString = String(repeating: "0", count: 3 - cardNumber.count).appending(cardNumber)
+        let path = "ui/icon/087000/087\(paddedString)_hr1.tex"
         let url = Endpoint.asset(at: path)
         
         return url
