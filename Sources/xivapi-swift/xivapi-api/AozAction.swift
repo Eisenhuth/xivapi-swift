d597faebf7b42cfe4c800561c57717baad694bd8
Jan Eisenhuth <47415874+Eisenhuth@users.noreply.github.com>
2024-07-30 05:01:41 +0200

    added more sheets

---
+++ Sources/xivapi-swift/xivapi-api/AozAction.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+public struct AozAction: Codable {
+    public let row_id: Int
+    public let fields: AozActionFields
+    public let transient: AozActionTransientFields
+}
+
+public extension AozAction {
+    var name: String { fields.Action.name }
+    var number: Int { transient.Number }
+    var category: ActionCategory { fields.Action.category }
+    var actionIcon: Icon { fields.Action.icon }
+    var transientIcon: Icon { transient.Icon }
+    var description: String { transient.Description }
+}
+
+public struct AozActionFields: Codable {
+    public let Action: Action
+    public let Rank: Int
+}
+
+public struct AozActionTransientFields: Codable {
+    public let CauseBind: Bool
+    public let CauseBlind: Bool
+    public let CauseDeath: Bool
+    public let CauseHeavy: Bool
+    public let CauseInterrupt: Bool
+    public let CauseParalysis: Bool
+    public let CausePetrify: Bool
+    public let CauseSleep: Bool
+    public let CauseSlow: Bool
+    public let CauseStun: Bool
+    
+    public let Description: String
+    public let Icon: Icon
+    public let Location: ContentFinderCondition
+    public let LocationKey: Int
+    public let Number: Int
+    public let Stats: String
+    public let TargetsEnemy: Bool
+    public let TargetsSelfOrAlly: Bool
+}
---
+++ Sources/xivapi-swift/xivapi-api/AozActionTransient.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+public struct AozActionTransient: Codable {
+    public let row_id: Int
+    public let fields: AozActionTransientFields
+}
