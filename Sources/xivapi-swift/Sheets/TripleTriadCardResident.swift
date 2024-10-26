b82557c1352cd2529b235f2e77624144637fb740
Jan Eisenhuth <47415874+Eisenhuth@users.noreply.github.com>
2024-10-26 06:08:19 +0200

    Update TripleTriadCardResident.swift

--- Sources/xivapi-swift/Sheets/TripleTriadCardResident.swift
+++ Sources/xivapi-swift/Sheets/TripleTriadCardResident.swift
@@ -6,10 +6,10 @@ public struct TripleTriadCardResident: Codable {
 }
 
 public extension TripleTriadCardResident {
-    var top: Int { fields.Top }
-    var bottom: Int { fields.Bottom }
-    var left: Int { fields.Left }
-    var right: Int { fields.Right }
+    var top: String { fields.Top == 10 ? "A" : fields.Top.description }
+    var bottom: String { fields.Bottom == 10 ? "A" : fields.Bottom.description }
+    var left: String { fields.Left == 10 ? "A" : fields.Left.description }
+    var right: String { fields.Right == 10 ? "A" : fields.Right.description }
     var type: String { fields.TripleTriadCardType.name }
     var stars: Int { fields.TripleTriadCardRarity.fields.Stars }
 }
