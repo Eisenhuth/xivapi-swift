8df5e05682b66c300d205f7b4ef85951be2a390a
Jan Eisenhuth <47415874+Eisenhuth@users.noreply.github.com>
2025-03-07 02:07:54 +0100

    7.18 hotfix

--- Sources/xivapi-swift/xivapi/Verified.swift
+++ Sources/xivapi-swift/xivapi/Verified.swift
@@ -1,4 +1,4 @@
 import Foundation
 
-public let verifiedVersion = "7.18"
+public let verifiedVersion = "7.18x1"
 public let verifiedSchema = "exdschema@9942dd96c70dfbba55bbc4280da144e5cb410737-2024.11.06.0000.0000"
