cf05cad955e9212a286bd36649602633f7b1eeba
Jan Eisenhuth <47415874+Eisenhuth@users.noreply.github.com>
2025-08-05 17:05:05 +0200

    updated verified schema and version

--- Sources/xivapi-swift/xivapi/Verified.swift
+++ Sources/xivapi-swift/xivapi/Verified.swift
@@ -1,4 +1,4 @@
 import Foundation
 
-public let verifiedVersion = "7.25x3"
-public let verifiedSchema = "exdschema@2:rev:56e942fd410c35e917ea442506b9f2f838b45dee"
+public let verifiedVersion = "7.3"
+public let verifiedSchema = "exdschema@2:rev:6a5085f56918e526c457fd3e9dfd27d3572c72a7"
